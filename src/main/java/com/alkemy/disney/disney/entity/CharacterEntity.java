6b51d7d22401989bd4594e7d919e5031a76055c6
JaviierCarrillo <javiercamur98@gmail.com>
2022-07-21 20:51:31 -0500

    creation of entities in the database

---
+++ src/main/java/com/alkemy/disney/disney/entity/CharacterEntity.java
@@ -0,0 +1,32 @@
+package com.alkemy.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "character")
+@Getter
+@Setter
+public class CharacterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String image;
+    private String name;
+    private Integer age;
+    private Long weight;
+    private String history;
+
+    @ManyToMany(cascade = CascadeType.ALL, targetEntity = MovieEntity.class)
+    @JoinTable(
+            name = "character_movie",
+            joinColumns = @JoinColumn(name = "character_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id"))
+    private List<MovieEntity> movies = new ArrayList<>();
+
+}
---
+++ src/main/java/com/alkemy/disney/disney/entity/MovieEntity.java
@@ -0,0 +1,43 @@
+package com.alkemy.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "movie")
+@Getter
+@Setter
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String image;
+    private String title;
+
+    @Column(name = "creation_date")
+    @DateTimeFormat(pattern = "yyyy/mm/dd")
+    private LocalDate creationDate;
+
+    private Long qualification;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "genre_id", insertable = false, updatable = false)
+    private GenreEntity genre;
+
+    @Column(name = "genre_id", nullable = false)
+    private Long genreId;
+
+    @ManyToMany(cascade = CascadeType.ALL, targetEntity = MovieEntity.class)
+    @JoinTable(
+            name = "character_movie",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "character_id"))
+    private List<CharacterEntity> characters = new ArrayList<>();
+}
